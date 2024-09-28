e9be2b5c102ff0383eccc457256426e888184713
Divi Kapoor <divikapoor@10-17-49-226.dynapool.wireless.nyu.edu>
2024-09-27 20:46:29 -0400

    updated run.py

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 
@@ -6,6 +8,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world!'
 
+# New route for the /time path
+@app.route('/time')
+def show_time():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return current_time
 
 app.run(host='0.0.0.0',
         port=8080,
