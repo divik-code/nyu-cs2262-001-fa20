01f2882704716957e03480f502d9f8674b227278
Divi Kapoor <divikapoor@mac.mynetworksettings.com>
2024-10-10 16:39:03 -0400

    add dns_app

---
+++ dns_app/Fibonacci_Server/FS.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import socket
+
+app = Flask(__name__)
+
+# Define the route for the '/register' endpoint
+@app.route('/register', methods=['PUT'])
+def register():
+    # Get JSON payload from request body
+    data = request.get_json()
+    hostname = data['hostname']
+    ip = data['ip']
+    as_ip = data['as_ip']
+    as_port = data['as_port']
+
+    # Construct registeration message in DNS-like format
+    message = f"TYPE=A\nNAME={hostname}\nVALUE={ip}\nTTL=10\n"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Send registeration message to AS
+    sock.sendto(message.encode(), (as_ip, int(as_port)))
+
+    return jsonify({'message': 'Hostname registered!'}), 201
+
+# Define a route to calculate fibonacci number
+@app.route('/fibonacci')
+def fibonacci():
+    num = request.args.get('number')
+    if not num.isdigit() or int(num) < 0:
+        return jsonify({'error': 'Bad input'}), 400
+    
+    fib_value = calculate_fibonacci(int(num))
+    return jsonify({'Your fibonacci number': fib_value}), 200
+
+# Recursive function to calculate Fibonacci number
+def calculate_fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return calculate_fibonacci(n - 1) + calculate_fibonacci(n - 2)
+
+if __name__ == '__main__':
+    app.run(port=9090)
